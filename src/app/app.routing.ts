136d3cd548c15ba09bc5b699e98be9729efff5a8
Sebastian Camacho <sxcamacho@gmail.com>
2018-03-22 17:33:58 -0300

    adding the first lines

---
+++ src/app/app.routing.ts
@@ -0,0 +1,18 @@
+// import { Routes, RouterModule } from '@angular/router'
+// import {AuthGuard} from "./service/authGuard.service";
+
+// import { HomePageComponent } from './pages/home-page/home-page.component'
+// import { LogsPageComponent } from './pages/gyms-page/gyms-page.component'
+
+// const routes: Routes = [
+//     {
+//         path: '',
+//         component: HomePageComponent,
+//     },
+//     {
+//         path: 'clients/:clientId/logs',
+//         component: LogsPageComponent,
+//     },
+// ]
+
+// export const AppRouting = RouterModule.forRoot(routes, { useHash: false })
